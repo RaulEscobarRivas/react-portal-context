ac3335f412679224953827ec87855a9e3f61aec5
Raul Escobar <raul13escobar@gmail.com>
2018-09-16 23:39:41 -0300

    boxes with prop drilling

--- src/BoxesContextExample.js
+++ src/BoxesContextExample.js
@@ -1,10 +1,39 @@
 import React, { Component } from 'react';
 
+const White = ({ counter, increment }) => (
+	<div className='white'>
+		{counter}
+		<div className='button'>
+			<button onClick={increment}>Increment!</button>
+		</div>
+	</div>
+);
 
-const White = () => <div className='white'/>
-const Green = () => <div className='green'><White/></div>
-const Blue = () => <div className='blue'><Green /></div>
+const Green = props => (
+	<div className='green'>
+		<White {...props}/>
+	</div>
+);
 
-const Boxes = () => <Blue />
+const Blue = props => (
+	<div className='blue'>
+		{props.counter}
+		<Green {...props}/>
+	</div>
+);
+
+class Boxes extends Component {
+	state = {
+		counter: 0
+	};
+
+	increment = () => this.setState({ counter: this.state.counter + 1 });
+
+	render() {
+		return (
+			<Blue counter={this.state.counter} increment={this.increment}/>
+		);
+	}
+}
 
 export default Boxes;
