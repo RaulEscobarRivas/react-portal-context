8f5daf9892a4dfda0772594468613aaf9652e0e9
Raul Escobar <raul13escobar@gmail.com>
2018-09-17 00:08:17 -0300

    change app title

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React-Context playground</h1>
+          <h1 className="App-title">Welcome to React Portal & Context playground</h1>
         </header>
         <BoxesContextExample />
       </div>
